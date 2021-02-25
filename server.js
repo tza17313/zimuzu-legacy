31848f7a25a6b14cb6869abb5df010d8c10fd07d
rayleigh <rayleigh@baie.com.cn>
2021-02-25 18:10:18 +0800

    完善rss内容

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/rss/:id', (req, res) => {
-    const url="http://"+req.headers.host+req.url
+    const url = 'http://' + req.headers.host + req.url
     _q(req.params.id).then((result) => {
         const {s, f = 'MP4'} = req.query
         // f enum("APP","HDTV","MP4","WEB-1080P","WEB-720P","BD-1080P","BD-720P","4K-2160P")
@@ -31,13 +31,15 @@ app.get('/rss/:id', (req, res) => {
         let feed = new Feed({
             title: info.cnname + ' / ' + info.enname,
             link: url,
-            description: info.aliaisname || info.cnname,
+            generator:"zimuzu-legacy",
+            docs:"/rss/:id?s=1&f=MP4;  id为剧id，s为第几季，f为下载格式；  s为数字，比如1、2、3、4、5；  f可以为APP,HDTV,MP4,WEB-1080P,WEB-720P,BD-1080P,BD-720P,4K-2160P",
+            description: info.aliaisname || info.cnname
         });
 
         let seasons = list
-        if (isNaN(s)) {
+        if (!isNaN(s)) {
             seasons = seasons.filter((season) => ~~s === ~~season.season_num)
-        }else{
+        } else {
             seasons.sort((a, b) => a.season_num - b.season_num)
         }
 
@@ -46,7 +48,7 @@ app.get('/rss/:id', (req, res) => {
             if (season.items[f]) {
                 //根据视频格式过滤
 
-                let sorted_ep=season.items[f].sort((a, b) => a.episode - b.episode)
+                let sorted_ep = season.items[f].sort((a, b) => a.episode - b.episode)
                 // 集数从小到大排序
                 for (let j = 0; j < sorted_ep.length; j++) {
                     let ep = sorted_ep[j]
@@ -54,9 +56,15 @@ app.get('/rss/:id', (req, res) => {
                         let file = ep.files[j]
                         if (~~file.way === 2) {
                             feed.addItem({
-                                link: url,
-                                enclosure_url: file.address,
-                                enclosure_type: 'application/x-bittorrent'
+                                id: ep.itemid,
+                                guid: ep.itemid + '_' + j,
+                                title: ep.name,
+                                enclosure: {
+                                    title: ep.name,
+                                    length:ep.size,
+                                    url: file.address,
+                                    type: 'application/x-bittorrent'
+                                }
                             })
                             break
                         }
